bc2beb72469f85f6ca1d2b873af1cce4d5ae0274
BeomChul <sbc0830@osstem.com>
2022-02-21 15:39:41 +0900

    Revise

--- applications/dml/src/views/Home.vue
+++ applications/dml/src/views/Home.vue
@@ -42,18 +42,4 @@ export default defineComponent({
 </script>
 
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
